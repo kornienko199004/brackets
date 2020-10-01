1eb1175d609fcaaa80ad97ec4ed1f8d42ab8ace6
Корниенко Алексей Александрович <Kornienko-A@gis.lan>
2020-10-01 21:52:50 +0300

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const strArr = str.split('');
+  let temp = [];
+  for (let i = 0; i < strArr.length; i += 1) {
+    const bracket = strArr[i];
+
+    for (let j = 0; j < bracketsConfig.length; j += 1) {
+      if (bracket === bracketsConfig[j][0]
+          && bracketsConfig[j][0] === bracketsConfig[j][1]) {
+            if (bracket === temp.slice(-1)[0]) {
+              temp = temp.slice(0, -1);
+            } else {
+              temp = [...temp, bracket];
+            }
+      }
+
+      if (bracket === bracketsConfig[j][0]) {
+        temp = [...temp, bracket];
+      }
+
+      if (bracket === bracketsConfig[j][1]
+          && temp.slice(-1)[0] === bracketsConfig[j][0]
+          && temp.length > 0) {
+        temp = temp.slice(0, -1);
+      } else if (bracket === bracketsConfig[j][1]) {
+          if (bracketsConfig[j][1] === bracketsConfig[j][0]) {
+            continue;
+          } else {
+            return false;
+          }
+      }
+    }
+  }
+
+  return temp.length === 0;
 }
